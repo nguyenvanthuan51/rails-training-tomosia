ea2ff8e7613ddaef028f4f95be52d674841bdbec
nguyenvanthuan51 <thuan.nguyen1@tomosia.com>
2020-02-12 11:12:14 +0700

    Done detail product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,14 @@ class ProductsController < ApplicationController
   def index
     @products = Product.page(params[:page]).per(PERPAGE)
   end
+  
+  def show
+    get_product
+  end
+
+  private
+
+  def get_product
+    @get_product = Product.find(params[:id])
+  end
 end
