e078b94c60afdfabdd35790d66de912f16b3833e
nguyenvanthuan51 <thuan.nguyen1@tomosia.com>
2020-02-14 16:58:01 +0700

    Edit price

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,7 +2,7 @@ class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product, foreign_key: :product_id
 
-  def total_price
+  def sub_total
     product.price.to_i * quantity.to_i
   end
 end
